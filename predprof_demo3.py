96c558dcb3de238398473d64e9f660a0e74e93c6
NadinTau <159414493+NadinTau@users.noreply.github.com>
2024-02-08 10:19:38 +0300

    Update predprof_demo3.py
    Исправлен список. Чтение осуществляется в общий список вместе с заголовком. При поиске ищем с 1 строки

--- predprof_demo3.py
+++ predprof_demo3.py
@@ -4,22 +4,21 @@ def readFile(filename):
     return list of students
     '''
     f = open(filename, 'r', encoding = 'utf-8')
-    global z1,z2,z3,z4,z5
-    z1,z2,z3,z4,z5=f.readline().strip().split(',')
     tab=[]
-    for i in range(500):
-        d1,d2,d3,d4,d5=f.readline().strip().split(',')
-        tab.append([d1,d2,d3,d4,d5]) 
+    for i in range(501):
+        d0,d1,d2,d3,d4=f.readline().strip().split(',')
+        tab.append([d0,d1,d2,d3,d4]) 
     return tab
+
 
 tab=readFile('students.csv')
 idproj=input()
 while idproj!="СТОП":
-    for i in range(len(tab)):
+    for i in range(1,len(tab)):
         fio=tab[i][1].split()
-        if tab[i][4]==idproj:
+        if tab[i][2]==idproj:
             
-            print(f'Проект №{tab[i][4]} делал: {fio[1][0]}.{fio[0]} он(а) получил(а) оценку - {tab[i][6]}.')
+            print(f'Проект №{tab[i][2]} делал: {fio[1][0]}.{fio[0]} он(а) получил(а) оценку - {tab[i][4]}.')
             break
     else:
         print('Ничего не найдено')
